84646c65aa62f6cca8e48fbbb227ede95df3a5db
xuanquoche <nguyentrinhxuanquoc2003@gmail.com>
2025-12-18 15:59:20 +0700

    refactor: deleted old folder

--- src/components/modules/header/header.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderActions, HeaderSearch } from '@/components/modules/header/index';
-export default function Header() {
-  return (
-    <header className='flex items-center justify-between w-full bg-white border-b px-4 py-2'>
-      <HeaderSearch />
-      <HeaderActions />
-    </header>
-  );
-}
--- src/components/modules/header/index.ts
+++
@@ -1,4 +0,0 @@
-export { AiButton } from './ai-button';
-export { HeaderActions } from './header-actions';
-export { HeaderSearch } from './header-search';
-export { UserAvatar } from './user-avatar';
