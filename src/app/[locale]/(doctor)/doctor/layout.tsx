37a281b0768ce2f247de205649c5a99ec557a4a4
xuanquoche <nguyentrinhxuanquoc2003@gmail.com>
2026-01-05 12:52:38 +0700

    refactor: Remove `title` prop from `PortalHeader` in doctor layout.

--- src/app/[locale]/(doctor)/doctor/layout.tsx
+++ src/app/[locale]/(doctor)/doctor/layout.tsx
@@ -9,7 +9,7 @@ const DoctorLayout = ({ children }: { children: React.ReactNode }) => (
     <div className='flex h-screen'>
       <PortalSidebar />
       <div className='w-[80%] overflow-y-auto'>
-        <PortalHeader title='Doctor Portal' badgeText='Doctor' />
+        <PortalHeader badgeText='Doctor' />
         {children}
       </div>
     </div>
